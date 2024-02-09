c1b80b9f70d558491cd768c7124f384a73e94063
Gareth Shippen <garethshippen@yahoo.co.uk>
2024-02-09 15:21:13 +0000

    Working with hardcoded file

--- logicHanger.py
+++ logicHanger.py
@@ -3,22 +3,22 @@ import copy
 from tkinter import *
 
 class Field():
-    def __init__(self, row, fieldname):
+    def __init__(self, row, fieldname_header):
         self.row = row
-        self.field_name = self.row[fieldname]
-        self.shown_by = self.set_shown_by()
-        self.shown_by_body = self.set_shown_by_body(self.shown_by)
+        self.field_name = self.row[fieldname_header]
+        self.shown_by = self.set_shown_by() # BL field contents
+        self.shown_by_body = self.set_shown_by_body(self.shown_by) # List of parents
         self.shows = []
 
-    def set_shown_by(self):
+    def set_shown_by(self): # Strips [event-name] from the BL
         bl = "Branching Logic (Show field only if...)"
         exact = self.row[bl].replace("[event-name]","")
         return exact
     def get_shown_by(self):
         return self.shown_by
     
-    def set_shown_by_body(self,text):
-        def body(text):
+    def set_shown_by_body(self,text): # Returns a list of parents
+        def body(text): # Extracts the field name of the parent field
             if text[0] == "(":
                 text = text[1:]
             start = text.find('[')
@@ -39,67 +39,81 @@ class Field():
     def get_shows(self):
         return self.shows
     
+    def set_field_name(self, fieldname):
+        self.field_name = fieldname
     def get_field_name(self):
         return self.field_name
     def show_field(self):
         print("Field name: {}".format(self.field_name))
         print("Shown by: {}".format(self.shown_by))
         print("Shown by body: {}".format(self.shown_by_body))
-        print("Shows: {}".format(self.shows)) # trying to find this out
+        print("Shows: {}".format(self.shows)) # TODO
+        print()
 
-    
-def read_csv(input_file):
-    fields = {}
-    roots = []
-    with open(input_file) as inp:
-        data = csv.DictReader(inp)
-        for row in data:
-            field_name = list(row.keys())[0]
-            fields[row[field_name]] = Field(row, field_name)
-            if row["Branching Logic (Show field only if...)"] == "":
-                roots.append(row[field_name])
-    return fields, roots
-data, roots = read_csv("source.csv")
+def gen_lines(filename):
+    def store_logic(field, level, storage):
+        storage.append((level * "  " + level * "-" + field.get_field_name()))
+        children = field.get_shows()
+        if children:
+            for child in children:
+                store_logic(child, level + 1, storage)
 
-unknowns = []
-for key, value in data.items():
-    parents = value.get_shown_by_body()
-    for parent in parents:
-        try:
-            data[parent].add_shows(value)
-        except Exception:
-            unknowns.append((parent,key))
+    def read_csv(input_file):
+        fields = {}
+        roots = []
+        fieldnames = set()
+        with open(input_file) as inp:
+            data = csv.DictReader(inp)
+            for row in data:
+                fieldname_header = list(row.keys())[0] # == ï»¿"Variable / Field Name"
+                fields[row[fieldname_header]] = Field(row, fieldname_header) # Dictionary of the actual field name pointing to its Field object
+                if row["Branching Logic (Show field only if...)"] == "": # This field has no parent
+                    roots.append(row[fieldname_header])
+                fieldnames.add(row[fieldname_header])
+        return fields, roots, fieldnames
 
-def show_logic(field, level):
-    print(level * "\t" + level * "-" + field.get_field_name())
-    children = field.get_shows()
-    if children:
-        for child in children:
-            show_logic(child, level + 1)
+    ########################
+    ###### BEGIN MAIN ######
+    ########################
 
-""" for root in roots:
-    show_logic(data[root],  0)
-    print() """
+    data, roots, fieldnames = read_csv(filename)
 
-def store_logic(field, level, storage):
-    storage.append((level * "  " + level * "-" + field.get_field_name()))
-    children = field.get_shows()
-    if children:
-        for child in children:
-            store_logic(child, level + 1, storage)
+    # Get the field names that aren't defined in the data dictionary
+    parents = set()
+    for value in data.values():
+        shown_by = value.get_shown_by_body()
+        for shown in shown_by:
+            parents.add(shown)
+    unknowns = parents - fieldnames
 
-lines = []
-for root in roots:
-    store_logic(data[root],  0, lines)
+    # Create a blank Field
+    empty_field = copy.deepcopy(data[roots[0]])
+    empty_field.set_field_name("empty")
 
-
-""" output = "dependent_fields.txt"
-with open(output, "w") as out:
-    for line in lines:
-        out.write(line + "\n")
-    out.write("\nThe following fields depend on other fields that weren't found.\n")
+    # Add 'unknown' fields to data
     for unknown in unknowns:
-        out.write(unknown[1] + " depends on " + unknown[0] + "\n") """
+        new_field = copy.deepcopy(empty_field)
+        new_field.set_field_name(unknown)
+        data[unknown] = new_field
+        roots.append(unknown)
+
+    # Associate parents with children
+    unknowns = []
+    for key, value in data.items():
+        parents = value.get_shown_by_body()
+        for parent in parents:
+            try:
+                data[parent].add_shows(value)
+            except Exception:
+                unknowns.append((parent,key))
+
+    lines = []
+    for root in roots:
+        store_logic(data[root],  0, lines)
+    
+    return lines
+
+lines = gen_lines('source.csv')
 
 #################
 ### BEGIN GUI ###
@@ -125,7 +139,10 @@ optionmenu.add_checkbutton(label="Verbose Mode")
 yscroll = Scrollbar(window)
 yscroll.pack(side=RIGHT, fill=Y)
 
-text = Text(window,yscrollcommand = yscroll.set)
+xscroll = Scrollbar(window, orient='horizontal')
+xscroll.pack(side=BOTTOM, fill=X)
+
+text = Text(window,yscrollcommand = yscroll.set, xscrollcommand=xscroll.set, wrap=NONE)
 text.pack(side=LEFT, fill = BOTH)
 text.tag_config('one', foreground = "red")
 text.tag_config('two', foreground = "blue")
@@ -144,11 +161,9 @@ for line in lines:
             text.insert(END, line + "\n", 'four')
         case _:
             text.insert(END, line + "\n")
-for unknown in unknowns:
-    text.insert(END, "\n" + unknown[1] + " " + unknown[0])
 
 yscroll.config(command=text.yview)
-
+xscroll.config(command=text.xview)
 window.mainloop()
 
 ###############
